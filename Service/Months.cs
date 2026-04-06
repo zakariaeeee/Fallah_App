6e0035c3c18327d3ce96bc39c87522f5ae799b37
zakariaeeee <121618748+zakariaeeee@users.noreply.github.com>
2026-04-06 18:05:37 +0100

    Add files via upload

---
+++ Service/Months.cs
@@ -0,0 +1,21 @@
+﻿namespace Fallah_App.Service
+{
+    public class Months
+    {
+        public string monthName { get; set; }
+     
+        public static List<Months> getMonthNames()
+        {
+            List<Months> monthNames = new List<Months>();
+            for(int i=1;i<=12;i++)
+            {        
+            DateTime dateObj = new DateTime(2000, i, 1);
+            string monthName = dateObj.ToString("MMMM");
+            Months date = new Months();
+            date.monthName = monthName;
+            monthNames.Add(date);
+            }
+            return monthNames;
+        }
+    }
+}
---
+++ Test/MeteoTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Fallah_App.Service;
+using NUnit.Framework;
+
+namespace Fallah_App.Test
+{
+    [TestFixture]
+    public class MeteoTests
+    {
+      
+        [Test]
+        public async Task GetMeteo_ValidCoordinates_ReturnsWeatherData()
+        {
+            String latitude = "12.345";
+            String longitude = "67.890";
+
+            Meteo weatherData = await Meteo.getMeteo(latitude, longitude);
+
+            Assert.IsNotNull(weatherData);
+          
+        }
+
+        [Test]
+        public async Task GetMeteo_InvalidCoordinates_ReturnsNull()
+        {
+            String latitude = "123456789";
+            String longitude = "67.890";
+
+            var weatherData = await Meteo.getMeteo(latitude, longitude);
+
+            Assert.IsNull(weatherData);
+        }
+
+      
+    }
+}
